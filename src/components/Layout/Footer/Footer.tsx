f2b85445cfe3ef8b24377be2a6ee7873816b3bb0
Joonyoung Park <mogun99@snu.ac.kr>
2023-11-21 21:40:37 +0900

    🧡 Modified font weight on footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -7,20 +7,20 @@ export default function Footer() {
     <footer className="mt-auto flex flex-col bg-black pl-5">
       <div className="items-center">
         {/* line 1 */}
-        <div className="flex flex-row items-center pb-3 pt-5">
+        <div className="flex flex-row items-center pb-2 pt-4">
           <div className="w-[20px] sm:w-[28px]">
             <Image src={SNULogo} alt="snu-logo" width={10000} />
           </div>
-          <p className="pl-2 text-sm text-white sm:text-lg">
+          <p className="pl-2 text-sm font-light text-white sm:text-lg">
             서울대학교 연합전공 정보문화학 과제 아카이빙 웹사이트
           </p>
         </div>
         {/* line 2 */}
-        <div className="flex flex-row items-center pb-5 pt-3">
+        <div className="flex flex-row items-center pb-4 pt-2">
           <div className="w-[20px] sm:w-[28px]">
             <Image src={EmailLogo} alt="email" width={10000} />
           </div>
-          <p className="pl-2 text-sm text-white sm:text-lg">
+          <p className="pl-2 text-sm font-light text-white sm:text-lg">
             snuiscexhibition@gmail.com
           </p>
         </div>
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -3,9 +3,12 @@ import ISCLogo from "../../../assets/img/logo_archiving.gif";
 
 export default function Header() {
   return (
-    <header className="relative flex flex-row items-start justify-start">
-      <div className="">
-        <Image src={ISCLogo} alt="isc-logo" width={130} />
+    <header className="relative flex h-[60px] flex-row items-center justify-between">
+      <Image src={ISCLogo} alt="isc-logo" width={130} />
+      <div className="flex flex-row">
+        <div>HOME</div>
+        <div className="pl-3">ABOUT</div>
+        <div className="px-3">ARCHIVING</div>
       </div>
     </header>
   );
