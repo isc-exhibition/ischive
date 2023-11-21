76e9ebae3c8f4305dfb5976867dd53f683d5d556
Joonyoung Park <mogun99@snu.ac.kr>
2023-11-21 21:03:46 +0900

    🧡 Add footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -7,16 +7,16 @@ export default function Footer() {
     <footer className="mt-auto flex flex-col bg-black pl-5">
       <div className="items-center">
         {/* line 1 */}
-        <div className="flex flex-row items-center py-3">
-          <Image src={SNULogo} alt="SNULogo" />
-          <p className="text-white ">
+        <div className="flex flex-row items-center pb-3 pt-5">
+          <Image src={SNULogo} alt="SNULogo" width={28} />
+          <p className="pl-2 text-lg text-white ">
             서울대학교 연합전공 정보문화학 과제 아카이빙 웹사이트
           </p>
         </div>
         {/* line 2 */}
-        <div className="flex flex-row items-center py-3">
-          <Image src={EmailLogo} alt="email" />
-          <p className="text-white">snuiscexhibition@gmail.com</p>
+        <div className="flex flex-row items-center pb-5 pt-3">
+          <Image src={EmailLogo} alt="email" width={28} />
+          <p className="pl-2 text-lg text-white">snuiscexhibition@gmail.com</p>
         </div>
       </div>
     </footer>
