6ecec036c449da96595e6250288ee823948061de
Joonyoung Park <mogun99@snu.ac.kr>
2023-11-21 14:02:30 +0900

    🧡 Feat: Footer at the bottom

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -4,14 +4,16 @@ import EmailLogo from "../../../assets/img/email_small.png";
 
 export default function Footer() {
   return (
-    <footer className="h-[130px] mt-auto bg-black">
+    <footer className="mt-auto flex flex-col bg-black pl-5">
       <div className="items-center">
+        {/* line 1 */}
         <div className="flex flex-row items-center py-3">
           <Image src={SNULogo} alt="SNULogo" />
           <p className="text-white">
             서울대학교 연합전공 정보문화학 과제 아카이빙 웹사이트
           </p>
         </div>
+        {/* line 2 */}
         <div className="flex flex-row items-center py-3">
           <Image src={EmailLogo} alt="email" />
           <p className="text-white">snuiscexhibition@gmail.com</p>
