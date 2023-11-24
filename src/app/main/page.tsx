b5fae3c31753a15cced2d926c1e018da21129f5a
YULMU <83626845+yulmu-catsetki@users.noreply.github.com>
2023-11-24 13:47:52 +0900

    Update page.tsx

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,1 +1,22 @@
-;
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
+      <div className="z-10 w-full flex flex-col items-center justify-between font-mono text-sm">
+        {/* Container for the image */}
+        <div className="relative w-full max-w-3xl">
+          {/* The Image component */}
+          <Image
+            src="/chrISCmas.jpeg"
+            alt="chrISCmas"
+            layout="responsive"
+            width={100}
+            height={141}
+            priority
+          />
+        </div>
+      </div>
+    </main>
+  )
+}
