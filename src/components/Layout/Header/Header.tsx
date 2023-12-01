7ca8d7396db5bd404c17b51f52af0d50da5f42c3
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-01 20:29:29 +0900

    🧡 Add colors on navigation bar

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -4,11 +4,16 @@ Header.tsx:
     One of the children of Layout
 */
 
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import ISCLogo from "../../../assets/img/logo_archiving.gif";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathName = usePathname();
+
   return (
     /* header fixed at the top while scrolling */
     <header className="fixed z-50 h-[60px] w-full border-b-2 border-solid border-black bg-white">
@@ -20,15 +25,27 @@ export default function Header() {
         {/* Texts */}
         <div className="flex flex-row">
           {/* HOME */}
-          <div className="text-xl sm:text-2xl">
+          <div
+            className={`text-xl sm:text-2xl ${
+              pathName == "/" ? "text-[#FF5C00]" : ""
+            }`}
+          >
             <Link href="/">HOME</Link>
           </div>
           {/* ABOUT */}
-          <div className="pl-2 text-xl sm:pl-4 sm:text-2xl">
+          <div
+            className={`pl-2 text-xl sm:pl-4 sm:text-2xl ${
+              pathName == "/about" ? "text-[#FF5C00]" : ""
+            }`}
+          >
             <Link href="/about">ABOUT</Link>
           </div>
           {/* ARCHIVING */}
-          <div className="px-2 text-xl sm:px-4 sm:text-2xl">
+          <div
+            className={`px-2 text-xl sm:px-4 sm:text-2xl ${
+              pathName == "/archiving" ? "text-[#FF5C00]" : ""
+            }`}
+          >
             <Link href="/archiving">ARCHIVING</Link>
           </div>
         </div>
