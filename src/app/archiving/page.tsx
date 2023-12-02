1a131f520837e10fad7fb777a97302c4a8ddf4a3
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-02 18:33:23 +0900

    🧡 Add Tracks

--- src/app/archiving/page.tsx
+++ src/app/archiving/page.tsx
@@ -4,7 +4,6 @@ archiving/page.tsx:
   Shows all tracks and courses
 */
 
-import Image from "next/image";
 import Layout from "@/components/Layout/Layout";
 import SemesterSelect from "@/components/SemesterSelect/SemesterSelect";
 
@@ -17,8 +16,22 @@ export default async function Page() {
       <div className="border-t-2 border-solid border-black">
         <SemesterSelect></SemesterSelect>
       </div>
-      {/* div: container for tracks and courses */}
-      <div className="border-t-2 border-solid border-black"></div>
+      {/* div: container for HCI tracks and courses */}
+      <div className="border-t-2 border-solid border-black px-10 py-10">
+        <h4># HCI Track</h4>
+      </div>
+      {/* div: container for Media & Contents tracks and courses */}
+      <div className="px-10 py-10">
+        <h4># Media & Contents Track</h4>
+      </div>
+      {/* div: container for Technology tracks and courses */}
+      <div className="px-10 py-10">
+        <h4># Technology Track</h4>
+      </div>
+      {/* div: container for Project tracks and courses */}
+      <div className="px-10 py-10">
+        <h4># Project</h4>
+      </div>
     </Layout>
   );
 }
