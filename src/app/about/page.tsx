0d3f3c0789a024aaa2ff1735664f6923a1410ef1
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-04 10:49:34 +0900

    🧡 Remove CreditsContainer, rename directory and modify docstring

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ about/page.tsx:
 
 import Image from "next/image";
 import Layout from "@/components/Layout/Layout";
-import ProfileContainer from "../../components/CreditsContainer/ProfileContainer";
+import ProfileContainer from "@/components/ProfileContainer/ProfileContainer";
 
 export default function About() {
   const pms = [
--- src/components/ProfileContainer/ProfileContainer.tsx
+++ src/components/ProfileContainer/ProfileContainer.tsx
@@ -1,3 +1,9 @@
+/* 
+ProfileContainer.tsx:
+  A container that contains profile image, 
+  team, major, name, and instagram account
+*/
+
 import Image from "next/image";
 
 export default function ProfileContainer({
@@ -17,6 +23,7 @@ export default function ProfileContainer({
 }) {
   return (
     <div className="my-6">
+      {/* profile image */}
       <div className="mx-5 w-40">
         <Image
           src={`/assets/img/profile/${team}_${name}.png`}
@@ -25,10 +32,13 @@ export default function ProfileContainer({
           height={10000}
         />
       </div>
+      {/* team */}
       <div className="my-2 text-center font-bold">{`${team}${
         isHead ? " 팀장" : ""
       }`}</div>
+      {/* major and name */}
       <div className="text-center">{`${major} ${name}`}</div>
+      {/* instagram or email account */}
       <div className="text-center text-[#FF5C00]">{account}</div>
     </div>
   );
