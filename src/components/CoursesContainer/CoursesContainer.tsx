b6467edf6f363a54cf423444e475e992bd1426ed
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-06 19:31:16 +0900

    🧡 Add Layout and SemesterSelect to archiving/[slug]

--- src/components/CoursesContainer/CoursesContainer.tsx
+++ src/components/CoursesContainer/CoursesContainer.tsx
@@ -4,12 +4,15 @@ CoursesContainer.tsx:
 */
 
 import { CourseType } from "@/api/courses";
+import Link from "next/link";
 
 function CourseButton({ course }: { course: CourseType }) {
   return (
-    <button className="my-2 mr-2 rounded-3xl border border-solid border-black px-6 py-2 font-Pretendard text-sm hover:border-[#FF5C00] hover:bg-[#FF5C00] hover:text-white md:text-base">
-      {course.name}
-    </button>
+    <Link href={`/archiving/${course.courseId}`}>
+      <button className="my-2 mr-2 rounded-3xl border border-solid border-black px-6 py-2 font-Pretendard text-sm hover:border-[#FF5C00] hover:bg-[#FF5C00] hover:text-white md:text-base">
+        {course.name}
+      </button>
+    </Link>
   );
 }
 
