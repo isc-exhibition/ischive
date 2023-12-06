4d89e881bf2c66bce24a824c09276d25c02f27e0
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-06 18:52:15 +0900

    🧡 Modify className of SemesterSelect

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const prefix =
     : "";
 
 const nextConfig = {
-  output: "export",
+  // output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
