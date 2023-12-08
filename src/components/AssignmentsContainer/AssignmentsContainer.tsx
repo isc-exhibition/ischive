42827678fd1728232105d22c1c87e1f23509f6eb
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-08 13:36:16 +0900

    🧡 Add AssignmentsContainer

--- src/components/AssignmentsContainer/AssignmentsContainer.tsx
+++ src/components/AssignmentsContainer/AssignmentsContainer.tsx
@@ -4,9 +4,28 @@ AssignmentsContainer.tsx:
 */
 
 import { AssignmentType } from "@/api/assignments";
+import { Asap_Condensed } from "next/font/google";
+import Image from "next/image";
 
-export function AssignmentButton({
-  assignment,
+function AssignmentButton({ assignment }: { assignment: AssignmentType }) {
+  return (
+    <div className="w-2">
+      <Image
+        src={assignment.thumbnail}
+        alt="thumbnail"
+        width={10000}
+        height={10000}
+      />
+    </div>
+  );
+}
+
+export default function AssignmentsContainer({
+  assignments,
 }: {
-  assignment: AssignmentType;
-}) {}
+  assignments: AssignmentType[];
+}) {
+  return assignments.map((assignment) => (
+    <AssignmentButton assignment={assignment} key={assignment.id} />
+  ));
+}
