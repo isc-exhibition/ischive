3058f54cb768471ba90c19477d0e71a225e0ddd0
HHCHU <ded06031@snu.ac.kr>
2023-12-14 22:08:21 +0900

    🧡 Fix chrISCmas screen ratio

--- src/app/chriscmas/page.tsx
+++ src/app/chriscmas/page.tsx
@@ -1,21 +1,39 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [koreanDate, setKoreaDate] = useState('');
+  const [koreanTime, setKoreanTime] = useState('');
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      const koreanDate = now.toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' });
+      const koreanTime = now.toLocaleTimeString('ko-KR', { timeZone: 'Asia/Seoul' });
+      setKoreaDate(koreanDate);
+      setKoreanTime(koreanTime);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
-      <div className="font-mono z-10 flex w-full flex-col items-center justify-between text-sm">
-        {/* Container for the image */}
-        <div className="relative w-full max-w-3xl">
-          {/* The Image component */}
+    <main className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full overflow-x-auto overflow-y-auto flex justify-center items-start bg-white">
+      <div className="w-[408px] h-auto pt-16 pb-16 bg-[rgb(77,101,169)] rounded-xl relative box-border flex flex-col items-center justify-center">
+        <div className="w-full h-full rounded-xl">
           <Image
             src="/assets/img/chrISCmas.jpeg"
             alt="chrISCmas"
             layout="responsive"
+            objectFit="cover"
             width={100}
             height={141}
             priority
           />
         </div>
+        <p className="text-center mt-2 text-white lg">{koreanDate}    {koreanTime}</p>
       </div>
     </main>
   );
