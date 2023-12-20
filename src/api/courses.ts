847c9419b1ff06da648dae0949320cd072aa4fd5
Joonyoung Park <mogun99@snu.ac.kr>
2023-12-21 03:27:24 +0900

    🧡 Add 커질방 to courses.ts

--- src/api/courses.ts
+++ src/api/courses.ts
@@ -58,6 +58,7 @@ export const courses: CoursesType = {
       { name: "디지털 에쓰노그래피", courseId: 3 },
       { name: "사용자중심디자인", courseId: 4 },
       { name: "디자인 사고와 커뮤니케이션", courseId: 5 },
+      { name: "커뮤니케이션 질적방법론", courseId: 30 },
     ],
     mediaContents: [
       { name: "영상문화입문", courseId: 6 },
@@ -94,6 +95,7 @@ export const courses: CoursesType = {
     hci: [
       { name: "HCI 이론 및 실습", courseId: 1 },
       { name: "사용자중심디자인", courseId: 4 },
+      { name: "커뮤니케이션 질적방법론", courseId: 30 },
     ],
     mediaContents: [
       { name: "디지털영상실습2", courseId: 8 },
@@ -136,21 +138,16 @@ export const courses: CoursesType = {
     ],
     mediaContents: [
       { name: "디지털영상실습2", courseId: 8 },
-
       { name: "사운드 인터랙션", courseId: 10 },
-
       { name: "인터랙티브 스토리텔링", courseId: 12 },
-
       { name: "시리어스 게임", courseId: 14 },
       { name: "가상현실입문", courseId: 15 },
-
       { name: "미래뉴스실습2", courseId: 28 },
     ],
     technology: [
       { name: "정보문화기술입문", courseId: 18 },
       { name: "정보구조", courseId: 19 },
       { name: "데이터 저널리즘", courseId: 20 },
-
       { name: "인터랙티브 미디어", courseId: 22 },
     ],
     project: [{ name: "산학연구실습", courseId: 25 }],
@@ -164,11 +161,8 @@ export const courses: CoursesType = {
     mediaContents: [
       { name: "영상문화입문", courseId: 6 },
       { name: "디지털영상실습1", courseId: 7 },
-
       { name: "디지털 음향의 이해", courseId: 9 },
-
       { name: "디지털 미디어의 이해", courseId: 11 },
-
       { name: "게임의 이해", courseId: 13 },
     ],
     technology: [
