749ba05ed0d6edca43e8b105d2de70df73373350
HHCHU <ded06031@snu.ac.kr>
2023-12-21 12:00:33 +0900

    🎄 Really ready for chrISCmas

--- src/app/chriscmas/page.tsx
+++ src/app/chriscmas/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="flex h-full w-full flex-col items-center bg-[rgb(77,101,169)]">
+    <div className="flex flex-col items-center bg-[rgb(187,200,232)]">
       <div className="h-auto w-[408px]">
         <Image
           src={"/assets/img/chriscmas/pamphlet.png"}
@@ -13,6 +13,18 @@ export default function Page() {
           className="relative h-full w-full"
         />
       </div>
+      <a
+        href="/"
+        className="mb-4 whitespace-pre-wrap text-center font-SsurroundAir text-sm text-[rgb(111,140,198)] hover:text-white hover:underline"
+      >
+        🎁 지난 과제들 보러가기 🎁
+      </a>
+      <a
+        href="/chriscmas/calendar"
+        className="mb-12 whitespace-pre-wrap text-center font-SsurroundAir text-sm text-[rgb(111,140,198)] hover:text-white hover:underline"
+      >
+        🎄 지난 어드벤트 캘린더 보러가기 🎄
+      </a>
     </div>
   );
 }
