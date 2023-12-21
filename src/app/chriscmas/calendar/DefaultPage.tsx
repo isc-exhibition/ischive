31ce8a8a64ddcd7779025b7f8397b38cf8c2eee4
HHCHU <ded06031@snu.ac.kr>
2023-12-21 15:32:15 +0900

    🎄 Add random feat for chrISCmas

--- src/app/chriscmas/calendar/DefaultPage.tsx
+++ src/app/chriscmas/calendar/DefaultPage.tsx
@@ -10,32 +10,43 @@ export default function DefaultPage() {
   const [currentMessage, setCurrentMessage] = useState("");
 
   useEffect(() => {
-    const today = new Date();
-    const date = today.getDate();
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
 
-    switch (date) {
-      case 17:
+    switch (randomNumber) {
+      case 1:
+        setCurrentImage("/assets/img/chriscmas/1215.png");
+        setCurrentMessage(
+          "바로 오늘이 과제전!<br/>크리슼마스(chrISCmas)에<br/>어서오세요!",
+        );
+        break;
+      case 2:
+        setCurrentImage("/assets/img/chriscmas/1216.png");
+        setCurrentMessage(
+          "이번 학기 과제와 팀플은 어떠셨나요?<br/>한 학기 동안 여러분들을 위해 준비한<br/>정문 엘프들의 선물(과제)들을 확인하러 오세요!",
+        );
+        break;
+      case 3:
         setCurrentImage("/assets/img/chriscmas/1217.png");
         setCurrentMessage(
           "학기 중에는 가지지 못했던 작은 여유,<br/>크리스마스까지 정무니우스와 함께<br/>가져보시는 건 어떠세요?",
         );
         break;
-      case 18:
+      case 4:
         setCurrentImage("/assets/img/chriscmas/1218.png");
         setCurrentMessage(
           "이번 학기 동안 여러분이 남긴 발자취<br/>정보문화학 과제전에서 함께 공유해주세요!<br/>남은 이틀, 기다리고 있을게요 ❄",
         );
         break;
-      case 19:
+      case 5:
         setCurrentImage("/assets/img/chriscmas/1219.png");
         setCurrentMessage(
           "여러분의 한 학기 어떠셨나요?<br/>이번 학기, 여러분이 기울인 노력이<br/>마치 하얀 눈이 쌓이듯,<br/>조금씩 쌓여 아름다운 겨울 풍경을<br/>이루고 있을 거예요.",
         );
         break;
-      case 20:
+      case 6:
         setCurrentImage("/assets/img/chriscmas/1220.png");
         setCurrentMessage(
-          "과제전까지 남은 날은 단 하루!<br/>즐거운 한 해의 마무리를<br/>chrISCmas<br/>와 함께 해주세요!",
+          "바로 오늘이 과제전!<br/>즐거운 한 해의 마무리를<br/>chrISCmas<br/>와 함께 해주세요!",
         );
         break;
       default:
