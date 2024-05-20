dc5857559ca2c1dd772627f2a683bb45b1cc1174
Joonyoung Park <jooony@snu.ac.kr>
2024-05-20 17:31:21 +0900

    [main] Add poster on 2024jungmoon24

--- src/app/2024jungmoon24/page.tsx
+++ src/app/2024jungmoon24/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Page() {
-  return <div>2024jungmoon24</div>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-[#DAC599]">
+      <div className="relative w-full max-w-md drop-shadow-lg md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+        <Image
+          src="/assets/img/2024jungmoon24/2024jungmoon24.png"
+          alt="Centered Image"
+          width={500}
+          height={500}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 }
