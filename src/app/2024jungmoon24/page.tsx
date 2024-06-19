a0f63d141a020503b7076aefb4d2a680bc401985
YULMU <83626845+yulmu-catsetki@users.noreply.github.com>
2024-06-19 14:31:26 +0900

    add 2024-1 pamplet

--- src/app/2024jungmoon24/page.tsx
+++ src/app/2024jungmoon24/page.tsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="flex h-screen items-center justify-center bg-[#DAC599]">
-      <div className="relative w-full max-w-md drop-shadow-lg md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+    <div className="flex flex-col items-center ">
+      <div className="h-auto w-[408px]">
         <Image
-          src="/assets/img/2024jungmoon24/2024jungmoon24.png"
-          alt="Centered Image"
-          width={500}
-          height={500}
-          className="object-contain"
+          src={"/assets/img/2024jungmoon24/2024-1_pamplet.png"}
+          alt="pamphlet"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="relative h-full w-full"
         />
       </div>
     </div>
   );
 }
+
