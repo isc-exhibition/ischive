59a18b44bd89b860be635931ecf6c199ffc55473
nuagenic <nuagenic@gmail.com>
2024-12-02 15:51:13 +0900

    refact: Add comments and delete unnecessary console logs

--- src/app/actions/uploadToGoogleSheet.ts
+++ src/app/actions/uploadToGoogleSheet.ts
@@ -3,6 +3,8 @@
 import { google } from "googleapis";
 
 export async function uploadToGoogleSheet(formData: FormData) {
+  // google service account를 통해 생성한 json credentials를 설정합니다.
+  // 이는 GET 요청은 기존처럼 API KEY로만 가능한 반면, POST 요청은 auth를 반드시 설정해야 하기 때문입니다.
   const auth = new google.auth.GoogleAuth({
     credentials: JSON.parse(
       process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON || "{}",
@@ -11,7 +13,7 @@ export async function uploadToGoogleSheet(formData: FormData) {
   });
   const sheets = google.sheets({ version: "v4", auth });
 
-  //   const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+  // 폼 제출을 받을 sheet id를 지정합니다.
   const SPREADSHEET_ID = process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID;
   // 폼 제출을 받을 탭 이름과 셀 범위를 지정합니다.
   const RANGE = "FormSubmittedAssignments(test)!A:N";
@@ -20,8 +22,6 @@ export async function uploadToGoogleSheet(formData: FormData) {
     [formData.get("name"), formData.get("email"), formData.get("message")],
   ];
 
-  //   console.log(values);
-  console.log(formValues);
   try {
     const response = await sheets.spreadsheets.values.append({
       spreadsheetId: SPREADSHEET_ID,
@@ -32,10 +32,8 @@ export async function uploadToGoogleSheet(formData: FormData) {
       },
     });
 
-    console.log("Successfully appended to Google Sheet:", response.data);
     return { success: true };
   } catch (error) {
-    console.error("Error appending to Google Sheet:", error);
     throw new Error("Failed to upload data to Google Sheet.");
   }
 }
