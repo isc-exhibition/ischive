61e4160d57cfba4a2dd58a956cd20d8989c1c654
snu-jmg <goldmango@snu.ac.kr>
2025-01-02 11:39:14 +0900

    add custom cursor

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,10 +5,11 @@ Layout.tsx:
 
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import "../../app/cursor.css";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col bg-white">
+    <div className="default-cursor flex min-h-screen flex-col bg-white">
       <Header />
       <main className="mt-[52px]">{children}</main>
       <Footer />
